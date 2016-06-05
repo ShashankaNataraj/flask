fc612c760a97cb95bdd6a9dd4e79a438728c42dd
Billy Kwong <tuppa@tuppa.org>
2016-06-05 23:16:08 +1000

    terraform variables cleanup and READM update

--- variables.tf
+++ variables.tf
@@ -1,12 +1,11 @@
 variable "public_key_path" {
   description = <<DESCRIPTION
-Path to the SSH public key to be used for authentication.
+Path to an existing SSH public key to be used for authentication.
 Ensure this keypair is added to your local SSH agent so provisioners can
 connect.
 
 Example: ~/.ssh/terraform.pub
 DESCRIPTION
-  default = "~/.ssh/kwongb-aws.pub"
 }
 
 variable "aws_credentials_file" {
